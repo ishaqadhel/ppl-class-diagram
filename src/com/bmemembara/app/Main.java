58d486a075352201d76ed03192d2f8d9eee5f16b
ishaqadhel <ishaq.adhel@gmail.com>
2021-11-01 20:24:01 +0700

    feat: add new data

--- src/com/bmemembara/app/Main.java
+++ src/com/bmemembara/app/Main.java
@@ -8,10 +8,10 @@ public class Main {
         Employee e = new Employee("3175022202190010", "Bill Harits", 'M', "22 February 1900", "Apple Street 21, Boston", 10000000.0, true);
         e.getEmployeeInformation();
 
-        Nurse n = new Nurse("3175021902199011", "Seo Dal Mi", 'M', "19 February 1900", "Apple Street 23, Boston", 7000000.0, true, "night");
+        Nurse n = new Nurse("3175021902199011", "Seo Dal Mi", 'W', "19 February 1900", "Apple Street 23, Boston", 7000000.0, true, "night");
         n.getNurseInformation();
 
-        Doctor d = new Doctor("3175023108199919", "Seo Dal Mi", 'M', "31 August 1999", "Melon Street 09, Boston", 18000000.0, true, "dermatologis");
+        Doctor d = new Doctor("3175023108199919", "Kim Jo Jo", 'W', "31 August 1999", "Melon Street 09, Boston", 18000000.0, true, "dermatologis");
         d.getDoctorInformation();
     }
 }
